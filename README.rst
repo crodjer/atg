455b2aef1480af058fb8a2ffc594a0e7dd7ee45e
Rohan Jain <crodjer@gmail.com>
2015-07-03 20:22:43 +0530

    Introduce LICENSE: GPLv3

--- README.rst
+++ README.rst
@@ -4,5 +4,3 @@ Around the Globe
  - Tells what time it is in a certain timezone/location.
  - Lets you know what a person may be doing at a given time.
  - Lets you figure out overlapping awake times.
-
-
--- setup.py
+++ setup.py
@@ -1,10 +1,18 @@
-"""A setuptools based setup module.
-
-See:
-https://packaging.python.org/en/latest/distributing.html
-https://github.com/crodjer/around-the-globe
-"""
-
+# atg: find what time is it around the globe
+# Copyright (C) 2015 Rohan Jain
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
